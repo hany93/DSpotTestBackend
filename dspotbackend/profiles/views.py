12e343b112b0553f3e2a28e18fde95e3e31f78f3
hany <hany93cuba@gmail.com>
2022-11-05 01:05:48 -0400

    add end point friends by profile

--- dspotbackend/profiles/views.py
+++ dspotbackend/profiles/views.py
@@ -1,4 +1,8 @@
+import re
+
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from profiles.models import Profile
 from profiles.serializers import ProfileSerializer
 
@@ -9,3 +13,22 @@ class ProfileViewSet(viewsets.ModelViewSet):
     """
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    @action(methods=['get'], detail=False, url_name='friends', url_path='friends')
+    def friends(self, request):
+        profile_id = request.GET.get('profile_id')
+        if not profile_id:
+            return Response('El identificador del perfil es requerido.', status=400)
+        else:
+            int_format = re.compile(r'^\-?[1-9][0-9]*$')
+            is_int = re.match(int_format, str(profile_id))
+            if not is_int:
+                return Response('El identificador del perfil debe ser un número entero.',
+                                status=400)
+
+        if Profile.objects.filter(pk=profile_id).exists():
+            friends = Profile.objects.get(pk=profile_id).friends
+            ser = ProfileSerializer(friends, many=True, context={'request': request})
+            return Response(ser.data, status=200)
+        else:
+            return Response('No existe perfil para el identificador ' + profile_id + '.', status=400)
