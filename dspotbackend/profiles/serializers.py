8345a45f62df7fd6b0b81dc4a7a8a3eb4c972629
hany <hany93cuba@gmail.com>
2022-11-05 23:26:17 -0400

    add field images to ProfileSerializer

--- dspotbackend/profiles/serializers.py
+++ dspotbackend/profiles/serializers.py
@@ -1,8 +1,22 @@
-from profiles.models import Profile
+from profiles.models import Profile, ProfilePhoto
 from rest_framework import serializers
 
 
+class ProfilePhotoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ProfilePhoto
+        fields = ['photo']
+
+
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    images = serializers.SerializerMethodField()
+
+    def get_images(self, instance):
+        images = instance.profilephotos
+        ser = ProfilePhotoSerializer(images, many=True)
+        return [image['photo'] for image in ser.data]
+
     class Meta:
         model = Profile
-        fields = '__all__'
+        fields = ['photo', 'first_name', 'last_name', 'phone', 'address', 'city', 'state', 'zipcode', 'bio', 'status',
+                  'available', 'friends', 'images']
