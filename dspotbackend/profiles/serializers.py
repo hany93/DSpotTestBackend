89e0d61ed44e68195291ebcf0ec170eb3fece2dd
hany <hany93cuba@gmail.com>
2022-11-07 03:23:56 -0500

    optimize ProfileSerializer

--- dspotbackend/profiles/serializers.py
+++ dspotbackend/profiles/serializers.py
@@ -2,21 +2,16 @@ from profiles.models import Profile, ProfilePhoto
 from rest_framework import serializers
 
 
-class ProfilePhotoSerializer(serializers.HyperlinkedModelSerializer):
+class ProfilePhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProfilePhoto
         fields = ['photo']
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    images = serializers.SerializerMethodField()
-
-    def get_images(self, instance):
-        images = instance.profilephotos
-        ser = ProfilePhotoSerializer(images, many=True)
-        return [image['photo'] for image in ser.data]
+    profilephotos = ProfilePhotoSerializer(many=True)
 
     class Meta:
         model = Profile
-        fields = ['id', 'photo', 'first_name', 'last_name', 'phone', 'address', 'city', 'state', 'zipcode', 'bio', 'status',
-                  'available', 'friends', 'images']
+        fields = ['id', 'photo', 'first_name', 'last_name', 'phone', 'address', 'city', 'state', 'zipcode', 'bio',
+                  'status', 'available', 'friends', 'profilephotos']
