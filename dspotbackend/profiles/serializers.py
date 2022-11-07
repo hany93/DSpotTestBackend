c84634eb3c7215ec186361f4daf687f078e1d9ed
hany <hany93cuba@gmail.com>
2022-11-07 03:45:07 -0500

    add django-cors-headers and its configurations, add a method for return status text

--- dspotbackend/profiles/serializers.py
+++ dspotbackend/profiles/serializers.py
@@ -10,13 +10,16 @@ class ProfilePhotoSerializer(serializers.HyperlinkedModelSerializer):
 
 class ProfileSerializer(serializers.ModelSerializer):
     images = serializers.SerializerMethodField()
+    textStatus = serializers.SerializerMethodField()
 
     def get_images(self, instance):
         images = instance.profilephotos
         ser = ProfilePhotoSerializer(images, many=True)
         return [image['photo'] for image in ser.data]
+    def get_textStatus(self, instance):
+        return instance.get_status_display()
 
     class Meta:
         model = Profile
         fields = ['id', 'photo', 'first_name', 'last_name', 'phone', 'address', 'city', 'state', 'zipcode', 'bio', 'status',
-                  'available', 'friends', 'images']
+                  'available', 'friends', 'images', 'textStatus']
