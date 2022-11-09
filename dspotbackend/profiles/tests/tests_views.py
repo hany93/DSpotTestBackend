b269e276744a968aa5a2dbec24e592b9b52d8fc9
hany <hany93cuba@gmail.com>
2022-11-09 02:01:33 -0500

    add tests list profiles and get profile by id

---
+++ dspotbackend/profiles/tests/tests_views.py
@@ -0,0 +1,33 @@
+from django.test import TestCase, Client as TestClient
+from profiles.models import Profile
+from model_bakery import baker
+
+
+class ProfileTest(TestCase):
+    def setUp(self):
+        domain = 'example.com'
+        self.client = TestClient(HTTP_ORIGIN=domain)
+
+    def test_list(self):
+        url = 'http://localhost:8000/profiles/profiles/'
+        for i in range(1, 10):
+            baker.make(Profile,
+                       first_name='Profile ' + str(i),
+                       last_name='Last Profile ' + str(i),
+                       city='City ' + str(i))
+        response = self.client.get(url)
+        data_response = response.json()
+        count = len(data_response)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(count, 9)
+
+    def test_get_profile_by_id(self):
+        url = 'http://localhost:8000/profiles/profiles/'
+        profile = baker.make(Profile,
+                             first_name='Profile',
+                             last_name='Last Profile',
+                             city='City ')
+        response = self.client.get(url + str(profile.id) + '/')
+        data_response = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(profile.id, data_response['id'])
